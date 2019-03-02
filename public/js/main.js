d713d89f88b6f64e9ec8f5cf02079ac5de4801fd
drtripathi <ddtripathi05@gmail.com>
2019-03-02 12:47:16 -0800

    new changes added

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,21 @@
 import SpriteSheet from './SpriteSheet.js';
-import {loadImage} from  './loaders.js';
+import {loadImage, loadLevel} from  './loaders.js';
 
 
+function drawBackground(background, context, sprites){
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+        //create the background image with sky sprite defined above
+        for(let x =x1; x < x2; ++x){
+            for(let y = y1; y < y2;  ++y){
+                sprites.drawTile(background.tile, context, x , y );
+            }
+        }
+    });
+}
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-context.fillRect(0,0,50,50);
 
 loadImage('/img/tiles.png')
 .then(image => {
@@ -14,6 +23,13 @@ loadImage('/img/tiles.png')
     const sprites = new SpriteSheet(image, 16, 16); //define size 16, 16 
     sprites.define('ground', 0, 0);// define location 0, 0
     sprites.define('sky', 3, 23); // define sky sprite
-    sprites.draw('sky', context, 45, 62); // draw the sprite on the context at location 45, 62
+
+    loadLevel('1-1')
+    .then(level => {
+        console.log(level); 
+        level.backgrounds.forEach(background => {
+        drawBackground(background, context, sprites);
+        }); 
+    }); 
 
 });
